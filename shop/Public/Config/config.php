d26925760a164ce6466546bd32b026a74036e955
gao <gao@qq.com>
2020-08-11 17:27:57 +0800

    0811

--- shop/Public/Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('HOST',"localhost");
-define('USER',"root");
-define('PWD',"root");
-define('DB',"ss30_shop");
-define('CHARSET','utf8');
--- shop/install/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include './install.class.php';
-
-$install = new install();
-
-$a = isset($_GET['a'])?$_GET['a']:'index';
-
-$install->$a();
